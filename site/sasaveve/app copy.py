13cec869f0713c16138a4aaca98d2755e9fe43a2
andreyundetected <andrey.undetected@gmail.com>
2026-01-05 11:34:08 +0100

    stub send_request_to_api

--- site/sasaveve/app copy.py
+++ site/sasaveve/app copy.py
@@ -1,3 +1,4 @@
+import requests
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -5,3 +6,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
+
+
+NEURO_API_URL = "http://127.0.0.1:5000/neuro_api"
+
+
+def send_request_to_api(payload):
+    print("Отправка запроса к API с payload:", payload)
+    response = requests.post(NEURO_API_URL, json=payload)
+    if response.status_code == 200:
+        print("API response:", response.json())
+        return response.json()
+    else:
+        print("Ошибка API:", response.text)
+        return {"error": "API Error", "details": response.text}
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    user_info = db.Column(db.JSON, default={})
+    course_info = db.Column(MutableList.as_mutable(db.JSON), default=[])
+    has_completed_interview = db.Column(db.Boolean, default=False)
