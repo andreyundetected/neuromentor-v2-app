fffdd186ef1c80ceb650e7726f4d4ef635ca533d
andreyundetected <andrey.undetected@gmail.com>
2026-02-06 11:26:41 +0100

    get_empty_course_info, require_login

---
+++ site/services/api_service.py
@@ -0,0 +1,21 @@
+
+
+async def get_empty_course_info():
+    return {
+        "0_topic": "",
+        "1_initial_level": "",
+        "2_target_level": "",
+        "3_name": "",
+        "4_structure": [
+            {
+                "0_topic": "",
+                "1_description": "",
+                "2_instructions_for_generating_lessons": "",
+                "3_lessons": [
+                    {"name": "", "description": ""},
+                    {"name": "", "description": ""}
+                ]
+            }
+        ],
+        "5_categories": []
+    }
--- site/services/user_service.py
+++ site/services/user_service.py
@@ -1,12 +1,4 @@
 from quart import session, redirect, url_for
 from models.user import User
 
-async def require_login():
-    if 'user_id' not in session:
-        return redirect(url_for('auth.login'))
-    if 'language' not in session:
-        session['language'] = 'ru'
-    user = await User.get(id=session['user_id'])
-    if not user:
-        return redirect(url_for('auth.login'))
-    return user
+
