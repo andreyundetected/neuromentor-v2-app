c3ceef1e29c435e835fbfe5533aa2d9591475624
andreyundetected <andrey.undetected@gmail.com>
2026-02-07 18:05:46 +0100

    cleanup transcription.py

--- site/transcription.py
+++ site/transcription.py
@@ -4,21 +4,11 @@ from pydub import AudioSegment
 from dotenv import load_dotenv
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-WHISPER_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions"
+
+
 WHISPER_MODEL = "whisper-1"
 
-def transcribe_audio_with_prepare_data(audio):
-    output_file_path = "temp_audio.wav"
-    audio.export(output_file_path, format="wav")
-    with open(output_file_path, "rb") as audio_file:
-        audio_data = audio_file.read()
-    try:
-        transcription = transcribe_audio(audio_data)
-    except Exception as e:
-        print(str(e))
-    os.remove(output_file_path)
-    return transcription
+
 
 def transcribe_audio(audio_data: bytes) -> str:
 
