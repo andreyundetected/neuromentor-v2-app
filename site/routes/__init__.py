6025a5dfe5a9cd6b30b648770d8e755c1e0c4faf
andreyundetected <andrey.undetected@gmail.com>
2026-02-07 17:04:36 +0100

    quick tweak app.py, __init__.py

--- site/routes/__init__.py
+++ site/routes/__init__.py
@@ -2,4 +2,4 @@ from .auth import auth_bp
 from .dashboard import dashboard_bp
 from .course import course_bp
 
-blueprints = [auth_bp, dashboard_bp, course_bp]
+blueprints = [auth_bp, dashboard_bp, course_bp, lesson_bp]
---
+++ site/routes/lesson.py
@@ -0,0 +1,4 @@
+from quart import Blueprint
+
+
+lesson_bp = Blueprint('lesson', __name__)
