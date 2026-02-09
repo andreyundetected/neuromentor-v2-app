110a7325295e4f2b1cee4c4a342c9b49fec731f1
andreyundetected <andrey.undetected@gmail.com>
2026-02-09 16:21:11 +0100

    transcription.py logic fix

--- site/transcription.py
+++ site/transcription.py
@@ -3,7 +3,6 @@ import requests
 from pydub import AudioSegment
 from dotenv import load_dotenv
 
-load_dotenv()
 
 
 
@@ -12,7 +11,4 @@ load_dotenv()
 
 
 
-if __name__ == "__main__":
-    input_file_path = "temp_audio.webm"
-    audio = AudioSegment.from_file(input_file_path, format="webm")
-    print(transcribe_audio_with_prepare_data(audio))
+
