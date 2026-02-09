84dc2edecb0a8622169a08abad183feddaaa84db
andreyundetected <andrey.undetected@gmail.com>
2026-02-09 16:03:34 +0100

    transcription.py minor fix

--- site/services/transcription.py
+++ site/services/transcription.py
@@ -1,3 +1,5 @@
+from pydub import AudioSegment
+from dotenv import load_dotenv
 import requests
 import os
 
@@ -41,3 +43,12 @@ def transcribe_audio_with_prepare_data(audio):
         print(str(e))
     os.remove(output_file_path)
     return transcription
+
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    input_file_path = "temp_audio.webm"
+    audio = AudioSegment.from_file(input_file_path, format="webm")
+    print(transcribe_audio_with_prepare_data(audio))
