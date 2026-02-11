a9402bd9fc70e63b19da58a807925c7db2e93a93
andreyundetected <andrey.undetected@gmail.com>
2026-02-11 16:42:47 +0100

    interview logic pass

--- site/app.py
+++ site/app.py
@@ -35,25 +35,7 @@ async def startup():
 async def shutdown():
     await Tortoise.close_connections()
 
-async def get_empty_course_info():
-    return {
-        "0_topic": "",
-        "1_initial_level": "",
-        "2_target_level": "",
-        "3_name": "",
-        "4_structure": [
-            {
-                "0_topic": "",
-                "1_description": "",
-                "2_instructions_for_generating_lessons": "",
-                "3_lessons": [
-                    {"name": "", "description": ""},
-                    {"name": "", "description": ""}
-                ]
-            }
-        ],
-        "5_categories": []
-    }
+
 
 
 
--- site/routes/interview.py
+++ site/routes/interview.py
@@ -1,3 +1,10 @@
+from services.api_service import send_request_to_api
+from models.user import User
+from quart import session
+from quart import url_for
+from quart import redirect
+from quart import request
+from quart import render_template
 from services.require_login import require_login
 from quart import jsonify
 from quart import Response
@@ -14,3 +21,42 @@ async def check_interview_status():
         return jsonify({"has_completed_interview": False})
     
     return jsonify({"has_completed_interview": user.has_completed_interview})
+
+
+async def interview():
+    if 'user_id' not in session:
+        return redirect(url_for("auth.login"))
+
+    user = await User.get(id=session['user_id'])
+
+    if request.method == 'POST':
+        form = await request.form
+        conversation_text = form['conversation']
+        conversation = session.get('conversation', [])
+        conversation.append({"role": "user", "content": conversation_text})
+
+        payload = {
+            "0_content": {
+                "0_conversation": conversation,
+                "1_user_info": user.user_info
+            },
+            "1_type": "interview"
+        }
+        response = await send_request_to_api(payload)
+
+        if response.get("response"):
+            conversation.append({"role": "manager", "content": response["response"]})
+
+        session['conversation'] = conversation
+
+        if response.get("user_info"):
+            print("Обновление user_info в базе данных")
+            user.user_info = response["user_info"]
+            if "<END>" in response.get("status"):
+                user.has_completed_interview = True
+            await user.save()
+
+        return jsonify(response)
+
+    session['conversation'] = []  
+    return await render_template('interview.html', username=user.username)
